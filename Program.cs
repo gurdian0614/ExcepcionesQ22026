df2516cd45572f83fccd024c60b98428ee9550ef
gurdian0614 <ivan.gurdian@outlook.com>
2026-04-24 19:23:59 -0600

    Adding list and dictionaries

--- Program.cs
+++ Program.cs
@@ -14,8 +14,41 @@
     //Console.WriteLine("La división de " + n1 + " y " + n2 + " es: " + division);
     Console.WriteLine($"La división de {n1} / {n2} es: {division}"); // Utiliza interpolación de cadenas para mostrar el resultado
 
+    List<string> frutas = new List<string>();
+    frutas.Add("Manzana");
+    frutas.Add("Banana");
+    frutas.Add("Naranja");
+    frutas.Add("Uva");
+    frutas.Insert(0, "Pera");
+    frutas.Remove("Manzana");
+
+    foreach (string fruta in frutas)
+    {
+        Console.WriteLine(fruta);
+        
+    }
+    Console.WriteLine($"Posición de la banana: {frutas.IndexOf("Banana")}");
+
+    Dictionary<string, int> inventario = new Dictionary<string, int>();
+    inventario.Add("Uva", 50);
+    inventario.Add("Banana", 20);
+    inventario.Add("Manzana", 30);
+    inventario.Remove("Banana");
+
+    foreach (var inv in inventario)
+    {
+        Console.WriteLine($"Fruta: {inv.Key}, Cantidad: {inv.Value}");
+    }
 }
-catch (Exception ex)
+catch (FormatException ex)
 {
-    Console.WriteLine("Ocurrió un error: " + ex.Message); // Muestra el mensaje de error si ocurre una excepción
+    Console.WriteLine("Ocurrió un error de tipo Formato: " + ex.Message); // Muestra el mensaje de error si ocurre una excepción
+}
+catch(Exception ex)
+{
+    Console.WriteLine("Ocurrió un error inesperado: " + ex.Message);
+}
+finally
+{
+    Console.WriteLine("Fin del Programa");
 }
