4b5c4d88a28068c18ec405012a00b6da766f1ba2
gurdian0614 <ivan.gurdian@outlook.com>
2026-04-24 18:11:09 -0600

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿try
+{
+    // Console.WriteLine: Muestra el mensaje en la consola
+    // Console.ReadLine: Lee la entrada del usuario desde la consola
+
+    Console.WriteLine("Ingrese primer número:");
+    double n1 = double.Parse(Console.ReadLine() ?? "0");// Lee la entrada del usuario y asigna "0" si es null
+
+    Console.WriteLine("Ingrese segundo número:");
+    double n2 = double.Parse(Console.ReadLine() ?? "0");
+
+    double division = n1 / n2; // Realiza la división
+
+    //Console.WriteLine("La división de " + n1 + " y " + n2 + " es: " + division);
+    Console.WriteLine($"La división de {n1} / {n2} es: {division}"); // Utiliza interpolación de cadenas para mostrar el resultado
+
+}
+catch (Exception ex)
+{
+    Console.WriteLine("Ocurrió un error: " + ex.Message); // Muestra el mensaje de error si ocurre una excepción
+}
